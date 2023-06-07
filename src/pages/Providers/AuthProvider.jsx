de030a29b9fc0612b6c296982ba349b3f95affd3
MostofaAsik <mmostofaasik@gmail.com>
2023-06-07 18:44:31 +0600

    'authprovider'

--- src/pages/Providers/AuthProvider.jsx
+++ src/pages/Providers/AuthProvider.jsx
@@ -1,17 +1,17 @@
-// import React, { createContext } from 'react';
+import React, { createContext } from 'react';
 
 
-// export const AuthContext = createContext(null)
-// const AuthProvider = ({ children }) => {
-//     const user = 'mas'
-//     const authInfo = {
-//         user
-//     }
-//     return (
-//         <AuthContext.Provider value={authInfo}>
-//             {children}
-//         </AuthContext.Provider>
-//     );
-// };
+export const AuthContext = createContext(null)
+const AuthProvider = ({ children }) => {
+    const user = 'mas'
+    const authInfo = {
+        user
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
 
-// export default AuthProvider;
+export default AuthProvider;
